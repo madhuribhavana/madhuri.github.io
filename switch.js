723b86fb26c201c74fa0ecabcad23271c0d16e87
dao-heart <danderayal@gmail.com>
2020-07-08 21:50:48 -0700

    Introduce animations

--- switch.js
+++ switch.js
@@ -1,10 +1,18 @@
-var textWrapper = document.querySelector(".rotate");
-var arr = ["design", "empathize", "code"];
-var randInt = anime.random(arr.length);
-anime.timeline({ loop: true }).add({
-  targets: textWrapper,
-  update: function () {
-    textWrapper.innerHTML = JSON.stringify(arr[randInt]);
-  },
-  easing: "easeOutElastic(1, .8)",
-});
+var i = 0;
+var arr = [
+  { first: "build", second: "stuff", emoji: "🔧" },
+  { first: "push", second: "pixels", emoji: "🖌️" },
+  { first: "empathize", second: "with users", emoji: "🔍" },
+  { first: "code", second: "stuff", emoji: "🖥️" },
+  { first: "design", second: "stuff", emoji: "✏️" },
+];
+
+function changeVal() {
+  if (i == arr.length) i = 0;
+  document.getElementById("first").innerHTML = arr[i].first;
+  document.getElementById("last").innerHTML = arr[i].second;
+  document.getElementById("emoji").innerHTML = arr[i].emoji;
+  i++;
+}
+
+setInterval(changeVal, 500);
